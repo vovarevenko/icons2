97cd994bc33e9e370aec543fce4cb95ee3147e11
Vova Revenko <vova@revenko.org>
2023-01-13 11:02:49 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as IconPin } from './icons/IconPin.vue'
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,30 @@
+// import vue2Jsx from '@vitejs/plugin-vue2-jsx'
+import { resolve } from 'node:path'
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
-import legacy from '@vitejs/plugin-legacy'
-import vue2 from '@vitejs/plugin-vue2'
+import { createVuePlugin } from 'vite-plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue2(),
-    legacy({
-      targets: ['ie >= 11'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    })
-  ],
+  plugins: [createVuePlugin({ jsx: true })],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'MyComponentLib',
+      fileName: format => `my-component-lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
